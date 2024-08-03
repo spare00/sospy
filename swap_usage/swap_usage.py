488d4504d18b54be81df6332a45223a30499badd
Brian Kim <>
2024-08-03 14:41:49 +0000

    Apply Hugepages usage to swap usage.
    System V IPC shared memory includes hugepages. Hugepages are reserved
    memory and not swappable. So the calculation hugepages usages should not
    be accounted when calculats swap usage.
    
    The output would be like:
    ```
    =======================================
               Swap Memory Usage
    =======================================
    
    From proc/meminfo:
      SwapTotal:           16777212 KB (  16.00 GB)
      SwapFree:            15530060 KB (  14.81 GB)
      Shmem:                2835316 KB (   2.70 GB)
      HugePages_Total:         6660 pages
      HugePages_Free:           105 pages
      Hugepagesize:            2048 KB
      Hugepages Used:      13424640 KB (  12.80 GB)
    
    From proc/sysvipc/shm:
      SysV VSS:            13424900 KB (  12.80 GB)
      SysV RSS:            13424708 KB (  12.80 GB)
      SysV Swapped:             156 KB (   0.00 GB)
    
    From df:
      tmpfs:                3681764 KB (   3.51 GB)
    
    Using above data:
      SwapTotal - SwapFree = Used Swap
      16777212 - 15530060 = 1247152 KB (1.19 GB)
    
      SysV RSS - Hugepages Used + tmpfs = Total swappable shared
      13424708 - 13424640 + 3681764 = 3681832 KB (3.51 GB)
    
      Total swappable shared - Shmem = Likely swapped-out shared
      3681832 - 2835316 = 846516 KB (0.81 GB)
    
      Used Swap - SysV Swapped = private (swapped from processes) + shared (Mostly swapped tmpfs)
      1247152 - 156 = 1246996 KB (1.19 GB)
    
      private (swapped from processes) + shared (Mostly swapped tmpfs) - Likely swapped-out shared = Likely private (swapped from processes)
      1246996 - 846516 = 400480 KB (0.38 GB)
    
    Thus, Swap usage (1.19 GB) would be like: private memory (0.38 GB) + shared memory (System V IPC: 0.00 GB + tmpfs: 0.81 GB)
    ```

--- swap_usage/swap_usage.py
+++ swap_usage/swap_usage.py
@@ -8,12 +8,18 @@ def swap_info():
         swap_total = re.search(r'SwapTotal:\s+(\d+) kB', content)
         swap_free = re.search(r'SwapFree:\s+(\d+) kB', content)
         shmem = re.search(r'Shmem:\s+(\d+) kB', content)
+        hugepages_total = re.search(r'HugePages_Total:\s+(\d+)', content)
+        hugepages_free = re.search(r'HugePages_Free:\s+(\d+)', content)
+        hugepagesize = re.search(r'Hugepagesize:\s+(\d+) kB', content)
 
         swap_total_value = int(swap_total.group(1)) if swap_total else 0
         swap_free_value = int(swap_free.group(1)) if swap_free else 0
         shmem_value = int(shmem.group(1)) if shmem else 0
+        hugepages_total_value = int(hugepages_total.group(1)) if hugepages_total else 0
+        hugepages_free_value = int(hugepages_free.group(1)) if hugepages_free else 0
+        hugepagesize_value = int(hugepagesize.group(1)) if hugepagesize else 0
 
-        return swap_total_value, swap_free_value, shmem_value
+        return swap_total_value, swap_free_value, shmem_value, hugepages_total_value, hugepages_free_value, hugepagesize_value
 
 def sysv_vss():
     with open('proc/sysvipc/shm', 'r') as file:
@@ -39,7 +45,7 @@ def tmpfs():
         return sum_tmpfs_kb
 
 if __name__ == "__main__":
-    swap_total_value, swap_free_value, shmem_value = swap_info()
+    swap_total_value, swap_free_value, shmem_value, hugepages_total_value, hugepages_free_value, hugepagesize_value = swap_info()
     sum_vss = sysv_vss()
     sum_rss = sysv_rss()
     sum_swapped = sysv_swapped()
@@ -57,38 +63,46 @@ if __name__ == "__main__":
     sum_swapped_gb = sum_swapped / (2**30)
     sum_tmpfs_gb = sum_tmpfs_kb / (2**20)
 
+    # Calculate Hugepages_Used
+    hugepages_used_value = (hugepages_total_value - hugepages_free_value) * hugepagesize_value
+    hugepages_used_gb = hugepages_used_value / (2**20)
+
     used_swap = swap_total_value - swap_free_value
-    total_shared = sum_rss_kb + sum_tmpfs_kb
-    likely_swapped_out_shared = total_shared - shmem_value
+    total_swappable_shared = sum_rss_kb - hugepages_used_value + sum_tmpfs_kb
+    likely_swapped_out_shared = total_swappable_shared - shmem_value
     processes_swapped_private_and_shared = used_swap - sum_swapped_kb
     processes_swapped_private = processes_swapped_private_and_shared - likely_swapped_out_shared
 
     print("=======================================")
-    print("           Swap Usage           ")
+    print("           Swap Memory Usage           ")
     print("=======================================\n")
 
     print("From proc/meminfo:")
-    print(f"   SwapTotal: {swap_total_value:>12} KB ({swap_total_gb:>7.2f} GB)")
-    print(f"    SwapFree: {swap_free_value:>12} KB ({swap_free_gb:>7.2f} GB)")
-    print(f"       Shmem: {shmem_value:>12} KB ({shmem_gb:>7.2f} GB)")
-
+    print(f"  SwapTotal:       {swap_total_value:>12} KB ({swap_total_gb:>7.2f} GB)")
+    print(f"  SwapFree:        {swap_free_value:>12} KB ({swap_free_gb:>7.2f} GB)")
+    print(f"  Shmem:           {shmem_value:>12} KB ({shmem_gb:>7.2f} GB)")
+    print(f"  HugePages_Total: {hugepages_total_value:>12} pages")
+    print(f"  HugePages_Free:  {hugepages_free_value:>12} pages")
+    print(f"  Hugepagesize:    {hugepagesize_value:>12} KB")
+    print(f"  Hugepages Used:  {hugepages_used_value:>12} KB ({hugepages_used_gb:>7.2f} GB)")
+    
     print("\nFrom proc/sysvipc/shm:")
-    print(f"    SysV VSS: {sum_vss_kb:>12} KB ({sum_vss_gb:>7.2f} GB)")
-    print(f"    SysV RSS: {sum_rss_kb:>12} KB ({sum_rss_gb:>7.2f} GB)")
-    print(f"SysV Swapped: {sum_swapped_kb:>12} KB ({sum_swapped_gb:>7.2f} GB)")
+    print(f"  SysV VSS:        {sum_vss_kb:>12} KB ({sum_vss_gb:>7.2f} GB)")
+    print(f"  SysV RSS:        {sum_rss_kb:>12} KB ({sum_rss_gb:>7.2f} GB)")
+    print(f"  SysV Swapped:    {sum_swapped_kb:>12} KB ({sum_swapped_gb:>7.2f} GB)")
 
     print("\nFrom df:")
-    print(f"       tmpfs: {sum_tmpfs_kb:>12} KB ({sum_tmpfs_gb:>7.2f} GB)")
+    print(f"  tmpfs:           {sum_tmpfs_kb:>12} KB ({sum_tmpfs_gb:>7.2f} GB)")
 
     print("\nUsing above data:")
     print(f"  SwapTotal - SwapFree = Used Swap")
     print(f"  {swap_total_value} - {swap_free_value} = {used_swap} KB ({used_swap / (2**20):.2f} GB)")
 
-    print(f"\n  SysV RSS + tmpfs = Total shared")
-    print(f"  {sum_rss_kb} + {sum_tmpfs_kb} = {total_shared} KB ({total_shared / (2**20):.2f} GB)")
+    print(f"\n  SysV RSS - Hugepages Used + tmpfs = Total swappable shared")
+    print(f"  {sum_rss_kb} - {hugepages_used_value} + {sum_tmpfs_kb} = {total_swappable_shared} KB ({total_swappable_shared / (2**20):.2f} GB)")
 
-    print(f"\n  Total shared - Shmem = Likely swapped-out shared")
-    print(f"  {total_shared} - {shmem_value} = {likely_swapped_out_shared} KB ({likely_swapped_out_shared / (2**20):.2f} GB)")
+    print(f"\n  Total swappable shared - Shmem = Likely swapped-out shared")
+    print(f"  {total_swappable_shared} - {shmem_value} = {likely_swapped_out_shared} KB ({likely_swapped_out_shared / (2**20):.2f} GB)")
 
     print(f"\n  Used Swap - SysV Swapped = private (swapped from processes) + shared (Mostly swapped tmpfs)")
     print(f"  {used_swap} - {sum_swapped_kb} = {processes_swapped_private_and_shared} KB ({processes_swapped_private_and_shared / (2**20):.2f} GB)")
@@ -96,4 +110,4 @@ if __name__ == "__main__":
     print(f"\n  private (swapped from processes) + shared (Mostly swapped tmpfs) - Likely swapped-out shared = Likely private (swapped from processes)")
     print(f"  {processes_swapped_private_and_shared} - {likely_swapped_out_shared} = {processes_swapped_private} KB ({processes_swapped_private / (2**20):.2f} GB)")
 
-    print(f"\nThus, Swap usage ({used_swap / (2**20):.2f} GB) would be like: private memory ({processes_swapped_private / (2**20):.2f} GB) + SysV IPC swapped shared ({sum_swapped_gb:.2f} GB) + tmpfs swapped shared ({likely_swapped_out_shared / (2**20):.2f} GB)")
+    print(f"\nThus, Swap usage ({used_swap / (2**20):.2f} GB) would be like: private memory ({processes_swapped_private / (2**20):.2f} GB) + shared memory (System V IPC: {sum_swapped_gb:.2f} GB + tmpfs: {likely_swapped_out_shared / (2**20):.2f} GB)")
